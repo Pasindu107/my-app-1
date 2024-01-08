530aea359778ed962133e87721c08ee3deea6592
Pasindu107 <pasindubanukaw@gmail.com>
2024-01-08 14:17:52 +0530

    third commit

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 const customers = () => {
   return (
     <div className='bg-gray-100 min-h-screen'>
-        <div className='grid lg:grid-cols p-4'>
-            <div className='bg-white flex justify-between w-full border p-10 rounded-lg'>
-
+        <div className='flex justify-between p-4'>
+            <h2>Customers</h2>
+            <Link href={'/'}><h2>Pasindu</h2></Link>
+        </div>
+        <div className='p-4'>
+            <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
+                <div className='my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
+                    <span>Name</span>
+                    <span className='sm:text-left text-right'>Email</span>
+                    <span className='hidden md:grid'>Last order</span>
+                    <span className='hidden sm:grid'>Method</span>
+                </div>
             </div>
+
         </div>
     </div>
     
